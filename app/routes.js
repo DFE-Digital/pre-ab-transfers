fb66bb04276f074f97b57121be06ff3084a12432
Anne Chan <anneychan209@gmail.com>
2022-05-17 17:10:29 +0100

    new options page and nav updates

--- app/routes.js
+++ app/routes.js
@@ -23,6 +23,28 @@ router.get('/transfers/incoming-trust-search', (req, res) => {
   }
 })
 
+
+router.post('/type-answer', function (req, res) {
+  // Make a variable to give it the value from the radio buttons on the index page  
+  var Task = req.session.data['control-name']
+
+  // Check whether the variable matches a condition
+
+  if (Task == "conversions") { 
+    // Send user to next page 
+      res.redirect('https://academy-conversions.herokuapp.com/MVP/index')
+
+  } 
+  
+  else if (Task == "transfers") {
+  //send user to transfers prototype
+      res.redirect('version-4/dashboard-home')
+
+  }
+
+
+  })
+
 module.exports = router
 
 // Run this code when a form is submitted to 'risks-branching'
